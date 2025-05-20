e0fc6245fefb25fa7c5928555b7f39504b9121bd
moon90 <axegangmoon@gmail.com>
2025-05-20 06:02:20 +0600

    #update base url

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://localhost:7083/api',
+  //baseURL: 'https://localhost:7083/api',
+  baseURL:'https://rms.enlightenedpharma.net/api/'
 });
 
 api.interceptors.request.use((config) => {
